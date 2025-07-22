f5c0d6024f7be49488c01d927b78011144e7aa16
saleamlak234 <salamlak101@gmail.com>
2025-07-22 12:00:24 +0300

    Delete takeit directory

--- takeit/server/middleware/admin.js
+++
@@ -1,8 +0,0 @@
-const adminMiddleware = (req, res, next) => {
-  if (req.user.role !== 'admin') {
-    return res.status(403).json({ message: 'Access denied. Admin privileges required.' });
-  }
-  next();
-};
-
-module.exports = adminMiddleware;
